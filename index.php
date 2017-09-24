15adc25eac73f9f0e957b816a4f51d69879a2e7a
Nickolas Nikolic <subclass@gmail.com>
2017-09-24 07:05:26 -0500

    added crawler for experimentation

--- index.php
+++ index.php
@@ -11,21 +11,12 @@ class MyCrawler extends PHPCrawler
 		else $lb = "<br />";
 
 		// Print the URL and the HTTP-status-Code
-		echo "Page requested: ".$DocInfo->url." (".$DocInfo->http_status_code.")";
-
-		// Print the refering URL
-		echo "Referer-page: ".$DocInfo->referer_url;
-
-		// Print if the content of the document was be received or not
-		if ($DocInfo->received == true)
-			echo "Content received: ".$DocInfo->bytes_received." bytes";
-		else
-			echo "Content not received";
+		echo "Page requested: " . $DocInfo->url . " (" . $DocInfo->http_status_code . ")";
 
 		// Now you should do something with the content of the actual
 		// received page or file ($DocInfo->source), we skip it in this example
 
-		echo $DocInfo->links_found_url_descriptors;
+		echo $DocInfo->links_found_url_descriptors . ", ";
 
 		flush();
 	}
@@ -34,6 +25,8 @@ class MyCrawler extends PHPCrawler
 // Now, create a instance of your class, define the behaviour
 // of the crawler (see class-reference for more options and details)
 // and start the crawling-process.
+
+echo "[";
 
 $crawler = new MyCrawler();
 
@@ -55,3 +48,5 @@ $crawler->setTrafficLimit(1000 * 1024);
 
 // Thats enough, now here we go
 $crawler->go();
+
+echo "]";
