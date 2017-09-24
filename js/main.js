c171aee7c6f3bea35a6491049973f9917b50bb28
Nickolas Nikolic <subclass@gmail.com>
2017-09-24 08:47:01 -0500

    added crawler for experimentation

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $.get("api/index.php", function(data){
             var aLinks = data.wizz;
 
-            _.each(aLinks,function (i) {
+            _.map(aLinks,function (i) {
                 console.log(i);
                 $('section').append('<article>'+ i + '</article>');
             });
