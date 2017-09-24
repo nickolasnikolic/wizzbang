2d3ed08a99f91e44138f1caaf92be863deed67c7
Nickolas Nikolic <subclass@gmail.com>
2017-09-24 09:09:44 -0500

    added crawler for experimentation

--- api/index.php
+++ api/index.php
@@ -4,19 +4,13 @@ include( "../classes/PHPCrawl/libs/PHPCrawler.class.php" );
 // Extend the class and override the handleDocumentInfo()-method
 class MyCrawler extends PHPCrawler
 {
+	public $wizz = array();
 	function handleDocumentInfo($DocInfo)
 	{
-		// Just detect linebreak for output ("\n" in CLI-mode, otherwise "<br>").
-		if (PHP_SAPI == "cli") $lb = "\n";
-		else $lb = "<br />";
 
 		// Print the URL and the HTTP-status-Code
-		echo "'"."Page requested: " . $DocInfo->url . " (" . $DocInfo->http_status_code . ")";
-
-		// Now you should do something with the content of the actual
-		// received page or file ($DocInfo->source), we skip it in this example
-
-		echo $DocInfo->links_found_url_descriptors . "'".", ";
+		array_push($wizz, "Page requested: " . $DocInfo->url . " (" . $DocInfo->http_status_code . ")" .
+		                  $DocInfo->links_found_url_descriptors;
 
 		flush();
 	}
@@ -25,8 +19,6 @@ class MyCrawler extends PHPCrawler
 // Now, create a instance of your class, define the behaviour
 // of the crawler (see class-reference for more options and details)
 // and start the crawling-process.
-
-echo '{ "wizz":[';
 
 $crawler = new MyCrawler();
 
@@ -49,4 +41,4 @@ $crawler->setTrafficLimit(1000 * 1024);
 // Thats enough, now here we go
 $crawler->go();
 
-echo "]}";
+echo json_encode($crawler->wizz);
