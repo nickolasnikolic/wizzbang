0bfc77d38c8c11ff49ac665a36358adb5886d8ff
Nickolas Nikolic <subclass@gmail.com>
2017-09-24 06:12:42 -0500

    added crawler for experimentation

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
-$startURL = 'http://www.wikipedia.org';
-$depth = 6;
-//$username = 'YOURUSER';
-//$password = 'YOURPASS';
-$crawler = new crawler($startURL, $depth);
-//$crawler->setHttpAuth($username, $password);
-// Exclude path with the following structure to be processed
-//$crawler->addFilterPath('customer/account/login/referer');
-$crawler->run();
+include("classes/PHPCrawl_083/PHPCrawler.class.php");
+
+$crawler = new MyCrawler();
+$crawler->setURL("www.wikipedia.org");
+$crawler->addContentTypeReceiveRule("#text/html#");
+
+$crawler->go();
