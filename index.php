12dc8f80f8d263ba67366648914b4e9042e5968a
Nickolas Nikolic <subclass@gmail.com>
2017-09-24 08:26:52 -0500

    added crawler for experimentation

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html>
 	<head>
 		<title>wizzbang</title>
-		<script src="js/jquery-3.2.1.js"></script>
-        <script src="js/rellax.js"></script>
-		<script src="js/main.js"></script>
+		<script type="text/javascript" src="js/jquery-3.2.1.js"></script>
+        <script type="text/javascript" src="js/rellax.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
 	</head>
 	<body>
 
